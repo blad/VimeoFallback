d31b2d9eecb09182e39b1f3430541bda8149d6ce
Bladymir Tellez <btellez@gmail.com>
2014-08-30 14:35:41 -0700

    Initial implementation of the fallback url gem

---
+++ lib/vvideo_fallback.rb
@@ -0,0 +1,50 @@
+require 'net/http'
+require 'uri'
+
+class Vvideo_fallback
+  attr_accessor :response
+  attr_accessor :url
+  attr_accessor :url_status
+
+  def initialize(id)
+    @id = id
+    getVideoURL(id)
+  end
+
+  def getVideoURL(id)
+    uri = URI.parse("http://player.vimeo.com/video/#{id}/fallback?noscript")
+
+    req = Net::HTTP::Get.new(uri.path)
+    req.add_field("Referer","http://player.vimeo.com/video/#{id}")
+    req.add_field("User-Agent","Ruby-2.0.0")
+
+    @response = Net::HTTP.new(uri.hostname, uri.port).start {|http|
+        http.request(req)
+    }
+
+    if (@response.is_a?(Net::HTTPSuccess))
+      processResponse(@response.body)
+    else
+      @url = "connection error"
+    end
+
+    @url
+  end
+
+  def getStatus
+    @response.code.to_i
+  end
+
+  private
+
+  def processResponse(responseBody)
+    match = /id=try-link href="(?<url>.*?)"/.match(responseBody)
+    if (match != nil)
+      @url = "http://player.vimeo.com"+match[:url]
+      @url_status = true
+    else
+      @url = "error occurred"
+      @url_status = false
+    end
+  end
+end
---
+++ vvideo_fallback.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'vvideo_fallback'
+  s.version     = '0.0.1'
+  s.date        = '2014-08-29'
+  s.summary     = "Fetch the Fallback Video URL for a Vimeo Video."
+  s.description = "The gem fetches the fallback video url for a vimeo video. The URL returned will be to a raw video file."
+  s.authors     = ["Bladymir Tellez"]
+  s.email       = 'btellez@gmail.com'
+  s.files       = ["lib/vvideo_fallback.rb"]
+  s.homepage    = 'http://www.computerscience.io/gems/vvideo_fallback'
+  s.license     = 'Apache v2.0'
+end
